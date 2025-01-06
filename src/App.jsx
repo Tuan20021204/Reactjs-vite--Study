40d809871e67c168cafe5a794177488ef87daca3
Tuan20021204 <107702049+Tuan20021204@users.noreply.github.com>
2025-01-06 22:51:44 +0700

    video 29

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import MyComponent from './components/learn/MyComponent'
+import SecondComponent from './components/learn/SecondComponent'
 
-const MyComponent = () => {
-  return (
-    <div> VAT & Vu Anh Tuan </div>
-  );
-}
 //() => { }
 const App = () => {
   const [count, setCount] = useState(0)
@@ -25,6 +22,7 @@ const App = () => {
       </div>
       <h1>Hello World & Vu Anh Tuan</h1>
       <MyComponent/>
+      <SecondComponent/>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
---
+++ src/components/learn/MyComponent.jsx
@@ -0,0 +1,8 @@
+
+const MyComponent = () => {
+  return (
+    <div> VAT & Vu Anh Tuan </div>
+  );
+}
+
+export default MyComponent;
