61a0bb8bc1c390fc0a71373540492b0ebb5f6c2c
Tuan20021204 <107702049+Tuan20021204@users.noreply.github.com>
2025-01-07 21:06:28 +0700

    video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,13 +1,20 @@
 
 import './style.css';
 const MyComponent = () => {
+  //const VAT = "Tuan"; //string
+  //const VAT = 23; // number
+  //const VAT = true; //boolean
+  //const VAT = [1, 2, 3];// array
+  const VAT = {
+    name: `tuan`,
+    age: 23
+  };
   return (
     <>
-      <div> VAT & Vu Anh Tuan </div>
+      <div> {JSON.stringify(VAT)} & Vu Anh Tuan </div>
       <div className="child"
-        style={
-          { borderRadius: "10px" }
-          }>child</div>
+        style={{ borderRadius: "10px" }
+        }>child</div>
     </>
   );
 }
