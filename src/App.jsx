f5114a6a2143d9fe80283c19904f68e622e39409
Tuan20021204 <107702049+Tuan20021204@users.noreply.github.com>
2025-05-04 18:39:05 +0700

    video 52

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 import Header from './components/layout/header';
 import Footer from './components/layout/footer';
-import { Outlet } from 'react-router-dom';
+import { Outlet, } from 'react-router-dom';
 
 
 //() => { }
