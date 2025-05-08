21f00345ed944110251cc8214dd6a35246ab91c0
Tuan20021204 <107702049+Tuan20021204@users.noreply.github.com>
2025-05-08 21:03:35 +0700

    video 66

--- src/pages/users.jsx
+++ src/pages/users.jsx
@@ -1,7 +1,12 @@
+import UserForm from "../components/user/user.form";
+import UserTable from "../components/user/user.table";
 
-const UsersPage = () =>{
-    return(
-        <div>users page</div>
+const UsersPage = () => {
+    return (
+        <div style={{ padding: "20px" }}>
+            <UserForm />
+            <UserTable />
+        </div>
     )
 }
 export default UsersPage;
