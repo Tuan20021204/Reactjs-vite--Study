d70ac9e826df7d3df06df2dbd0bfd525187224ba
Tuan20021204 <107702049+Tuan20021204@users.noreply.github.com>
2025-05-19 22:29:31 +0700

    video 78

--- src/components/user/user.table.jsx
+++ src/components/user/user.table.jsx
@@ -1,14 +1,24 @@
 import { Table } from 'antd';
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
+import UpdateUserModal from './update.user.modal';
+import { useState } from 'react';
 
 
 const UserTable = (props) => {
-  const {dataUsers} = props;
+  const { dataUsers, loadUser } = props;
 
+  const [dataUpdate, setDataUpdate] = useState(null);
+  const [isModalUpdateOpen, setIsModalUpdateOpen] = useState(false)
   const columns = [
     {
       title: 'Id',
       dataIndex: '_id',
+      render: (_, record) => (
+        <>
+          <a href='#'> {record._id}</a>
 
+        </>
+      ),
     },
     {
       title: 'Full name',
@@ -19,16 +29,44 @@ const UserTable = (props) => {
       title: 'Email',
       dataIndex: 'email',
 
-    }
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      render: (_, record) => (
+        <div style={{ display: "flex", gap: "20px" }}>
+          <EditOutlined 
+          onClick={()=> {
+            setDataUpdate(record)
+            setIsModalUpdateOpen(true)}
+          }
+          style={{ cursor: "pointer", color: "orange" }} />
+          <DeleteOutlined style={{ cursor: "pointer", color: "red" }} />
+        </div>
+      ),
+
+    },
 
   ];
 
-  
 
-  return (<Table
-    columns={columns}
-    dataSource={dataUsers}
-    rowKey={"_id"} />
+
+  return (
+    <>
+      <Table
+        columns={columns}
+        dataSource={dataUsers}
+        rowKey={"_id"}
+      />
+      <UpdateUserModal
+      isModalUpdateOpen = {isModalUpdateOpen}
+      setIsModalUpdateOpen = {setIsModalUpdateOpen}
+      dataUpdate = {dataUpdate}
+      setDataUpdate = {setDataUpdate}
+      loadUser = {loadUser}
+      />
+    </>
+
   )
 }
 export default UserTable;
--- src/pages/users.jsx
+++ src/pages/users.jsx
@@ -17,7 +17,9 @@ const UsersPage = () => {
     return (
         <div style={{ padding: "20px" }}>
             <UserForm loadUser = {loadUser}/>
-            <UserTable dataUsers = {dataUsers}/>
+            <UserTable 
+            dataUsers = {dataUsers}
+            loadUser = {loadUser}/>
         </div>
     )
 }
